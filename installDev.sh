952b83a7872ade6235184c43e9ca42a8e14e6499
Ang3el <94862096+Vancltkin@users.noreply.github.com>
2025-01-17 10:48:28 +0300

    Create installDev.sh

---
+++ installDev.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+# Обновление репозиториев и установка зависимостей
+echo "Устанавливаю зависимости..."
+opkg update && opkg install openssh-sftp-server nano curl sing-box
+
+# Конфигурация sing-box
+echo "Настройка sing-box..."
+uci set sing-box.main.enabled="1"
+uci set sing-box.main.user="root"
+uci commit sing-box
+
+# Отключение сервиса sing-box
+echo "Отключаю сервис sing-box..."
+service sing-box disable
+
+# Очистка конфигурационного файла sing-box
+echo "Очищаю конфигурационный файл /etc/sing-box/config.json..."
+echo '{}' > /etc/sing-box/config.json
+
+# Создание нового интерфейса "proxy"
+echo "Создаю новый интерфейс proxy..."
+uci set network.proxy=interface
+uci set network.proxy.proto="none"
+uci set network.proxy.device="singtun0"
+uci set network.proxy.defaultroute="0"
+uci set network.proxy.delegate="0"
+uci set network.proxy.peerdns="0"
+uci set network.proxy.auto="1"
+uci commit network
+service network restart
+
+# Настройка правил файрвола
+echo "Настройка правил файрвола..."
+uci add firewall zone
+uci set firewall.@zone[-1].name="proxy"
+uci set firewall.@zone[-1].forward="REJECT"
+uci set firewall.@zone[-1].output="ACCEPT"
+uci set firewall.@zone[-1].input="ACCEPT"
+uci set firewall.@zone[-1].masq="1"
+uci set firewall.@zone[-1].mtu_fix="1"
+uci set firewall.@zone[-1].device="singtun0"
+uci set firewall.@zone[-1].family="ipv4"
+uci add_list firewall.@zone[-1].network="singtun0"
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].dest="proxy"
+uci set firewall.@forwarding[-1].src="lan"
+uci set firewall.@forwarding[-1].family="ipv4"
+uci commit firewall
+service firewall reload
+
+# Открытие конфигурационного файла для редактирования
+echo "Открываю конфигурационный файл для редактирования..."
+nano /etc/sing-box/config.json
+
+# Запрос на подтверждение, что файл настроен правильно
+while true; do
+    read -p "Вы настроили файл /etc/sing-box/config.json ? (y/n, по умолчанию y): " yn
+    yn=${yn:-y}  # Если пользователь не ввел ничего, по умолчанию будет 'y'
+    
+    case $yn in
+        [Yy]* ) 
+            echo "После записи /etc/sing-box/config.json перезапускаем службу"
+            service sing-box enable
+            service sing-box restart
+            break
+            ;;
+        [Nn]* ) 
+            echo "Перезапускаю редактор nano для редактирования конфигурации..."
+            nano /etc/sing-box/config.json
+            ;;
+        * ) 
+            echo "Вы настроили файл /etc/sing-box/config.json. Пожалуйста, введите y (да) или n (нет)."
+            ;;
+    esac
+done
+
+# Проверяем, существует ли файл config2.json, если нет - создаем пустой файл
+if [ ! -f /etc/sing-box/config2.json ]; then
+    echo "{}" > /etc/sing-box/config2.json
+    echo "Created /etc/sing-box/config2.json"
+fi
+
+# Проверяем, существует ли файл config3.json, если нет - создаем пустой файл
+if [ ! -f /etc/sing-box/config3.json ]; then
+    echo "{}" > /etc/sing-box/config3.json
+    echo "Created /etc/sing-box/config3.json"
+fi
+
+# Установка singb-ui
+echo "Установка singb"
+wget -O /root/luci-app-singb.ipk https://github.com/Vancltkin/singb/releases/download/v0.1.4c/luci-app-singb.ipk && chmod 0755 /root/luci-app-singb.ipk && opkg update && opkg install /root/luci-app-singb.ipk && /etc/init.d/uhttpd restart
+
+echo "Обновляем UI"
+rm -f /var/luci-indexcache*
+rm -f /tmp/luci-indexcache*
+
+# Перезагружаем rpcd, если он доступен
+[ -x /etc/init.d/rpcd ] && /etc/init.d/rpcd reload
+
+# Перезагружаем uhttpd, если он доступен
+[ -x /etc/init.d/uhttpd ] && /etc/init.d/uhttpd restart
